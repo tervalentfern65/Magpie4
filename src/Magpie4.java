8b26eb05cc57086f102b59faac74f5876b36950a
tervalentfern65 <joey1999tom@gmail.com>
2014-10-30 10:12:00 -0400

    done

--- src/Magpie4.java
+++ src/Magpie4.java
@@ -49,7 +49,7 @@ public class Magpie4
 		}
 
 		// Responses which require transformations
-		else if (findKeyword(statement, "I want to", 0) >= 0)
+		else if (findKeyword(statement, "I want", 0) >= 0)
 		{
 			response = transformIWantToStatement(statement);
 		}
@@ -58,10 +58,10 @@ public class Magpie4
 		{
 			// Look for a two word (you <something> me)
 			// pattern
-			int psn = findKeyword(statement, "you", 0);
+			int psn = findKeyword(statement, "I", 0);
 
 			if (psn >= 0
-					&& findKeyword(statement, "me", psn) >= 0)
+					&& findKeyword(statement, "you", psn) >= 0)
 			{
 				response = transformYouMeStatement(statement);
 			}
@@ -90,9 +90,9 @@ public class Magpie4
 			statement = statement.substring(0, statement
 					.length() - 1);
 		}
-		int psn = findKeyword (statement, "I want to", 0);
-		String restOfStatement = statement.substring(psn + 9).trim();
-		return "What would it mean to " + restOfStatement + "?";
+		int psn = findKeyword (statement, "I want", 0);
+		String restOfStatement = statement.substring(psn + 7).trim();
+		return "Would you really be happy if you had " + restOfStatement + "?";
 	}
 
 	
@@ -115,11 +115,11 @@ public class Magpie4
 					.length() - 1);
 		}
 		
-		int psnOfYou = findKeyword (statement, "you", 0);
-		int psnOfMe = findKeyword (statement, "me", psnOfYou + 3);
+		int psnOfYou = findKeyword (statement, "I", 0);
+		int psnOfMe = findKeyword (statement, "you", psnOfYou + 2);
 		
-		String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe).trim();
-		return "What makes you think that I " + restOfStatement + " you?";
+		String restOfStatement = statement.substring(psnOfYou + 2, psnOfMe).trim();
+		return "Why do you " + restOfStatement + " me?";
 	}
 	
 	
